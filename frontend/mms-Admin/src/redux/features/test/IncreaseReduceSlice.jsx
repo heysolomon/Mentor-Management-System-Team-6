d63aead24a2488e3e435f6f3080e689f81b2e0db
Solomon Akuson <akusonsolomon15@gmail.com>
2023-04-06 21:10:12 +0100

    codacy fix

--- frontend/mms-Admin/src/redux/features/test/IncreaseReduceSlice.jsx
+++ frontend/mms-Admin/src/redux/features/test/IncreaseReduceSlice.jsx
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
 
 // const initialState = {
 //   product: [],
@@ -6,23 +7,20 @@ import { createSlice } from "@reduxjs/toolkit";
 //   total: 0,
 // };
 
-
-
 const IncreaseDescreaseSlice = createSlice({
-  name: "drugs",
-  initialState: {count: 0},
+  name: 'drugs',
+  initialState: { count: 0 },
   reducers: {
-   addPercent: (state) => {
-    state.count += 1;
-   },
-   removePercent: (state) => {
-    state.count -= 1;
-   }
-    
+    addPercent: (state) => {
+      state.count += 1;
+    },
+    removePercent: (state) => {
+      state.count -= 1;
+    },
+
   },
- 
+
 });
 
-export const { addPercent, removePercent,  } = IncreaseDescreaseSlice.actions;
+export const { addPercent, removePercent } = IncreaseDescreaseSlice.actions;
 export default IncreaseDescreaseSlice.reducer;
-
