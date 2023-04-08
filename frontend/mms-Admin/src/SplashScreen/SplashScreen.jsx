d286df9398cada3603be50084af85f24e99fe24a
dinatahoun92 <dinatahoun992@gmail.com>
2023-04-08 02:37:52 +0200

    basic splash screen

---
+++ frontend/mms-Admin/src/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SplashScreen() {
+  return (
+    <div className='bg grid h-screen place-items-center'> <img src="/assets/logo.png"></img></div>
+  )
+}
--- frontend/mms-Admin/src/main.jsx
+++ frontend/mms-Admin/src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './pages.css'
 import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import store from './redux/store';
 import ErrorPage from './pages/Error/ErrorPage';
-
+import SplashScreen from './SplashScreen/SplashScreen';
 // This is were you add routes for the pages you are building
 const router = createBrowserRouter([
   {
     // your route goes here
     path: '/',
     // this is where you add the component of the page you are routing to
-    element: 'this is the landing page nwwwww',
+    element: <SplashScreen/>,
     // this is a fallback error page that appears and shows you the error of the particular route
     errorElement: <ErrorPage />,
   },
