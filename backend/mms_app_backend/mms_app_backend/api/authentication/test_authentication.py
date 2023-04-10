ae1502dc513532975e5704ef3212f1128933c8cc
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-10 09:44:08 +0200

    wrote test to test signup endpoint.

---
+++ backend/mms_app_backend/mms_app_backend/api/authentication/test_authentication.py
@@ -0,0 +1,12 @@
+from ....main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+post = client.post
+get = client.get
+
+
+def test_signup():
+    response = get('/signup')
+    assert response.status_code == 405
+    assert response.json() == {"detail":"Method Not Allowed"}
