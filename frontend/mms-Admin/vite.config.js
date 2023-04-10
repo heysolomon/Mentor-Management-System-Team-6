e2458646e5bba234371332c871d2887b1598723a
Solomon Akuson <akusonsolomon15@gmail.com>
2023-04-10 09:02:48 +0100

    updated this branch with the origin/develop branch, started building the navbar of the dashboard #3

--- frontend/mms-Admin/vite.config.js
+++ frontend/mms-Admin/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { // configured to allow Vite to work with docker
+    host: '0.0.0.0',
+    port: 3000, // This is the port which we will use in docker
+  },
 });
