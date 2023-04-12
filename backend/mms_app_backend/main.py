b4bc92934b2a1b1304f53dde640a6b8fc999a0e0
dvy-dev <david.chimzizi@gmail.com>
2023-04-12 09:38:24 +0200

    Backend cicd-setup- Self-Sprint-1

--- backend/mms_app_backend/main.py
+++ backend/mms_app_backend/main.py
@@ -1,24 +1,30 @@
-
+# Import FastAPI and RedirectResponse classes
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-if __name__ == "main":
-    from mms_app_backend.api.authentication.route import router as auth_router
-else:
-    from .mms_app_backend.api.authentication.route import router as auth_router
+# Import the authentication router
+from mms_app_backend.mms_app_backend.api.authentication.route import router as auth_router
 
-
+# Instantiate the FastAPI application
 app = FastAPI()
+
+# Include the authentication router in the FastAPI app
 app.include_router(auth_router)
+
+# Create shortcuts for app.get and app.post decorators
 get = app.get
 post = app.post
 
-
+# Define the root endpoint, which redirects to the API documentation
 @get('/')
 def homepage():
     """
-    This is the api's home page.
-    :return:
-
+    This is the API's home page.
+    :return: A RedirectResponse to the API documentation
     """
     return RedirectResponse('/redoc')
+
+# Run the Uvicorn server if this script is executed as the main module
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
