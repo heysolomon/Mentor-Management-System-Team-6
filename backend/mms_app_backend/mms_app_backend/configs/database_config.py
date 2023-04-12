7cef2501794275e2c95dd3acd3976f84c23f8dd3
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-12 07:58:34 +0200

    completed database and env configurations.

--- backend/mms_app_backend/mms_app_backend/configs/database_config.py
+++ backend/mms_app_backend/mms_app_backend/configs/database_config.py
@@ -1,10 +1,19 @@
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/mms_database"
+load_dotenv()
+
+username = os.environ['DATABASE_USERNAME']
+database = os.environ['DATABASE_NAME']
+host = os.environ['DATABASE_HOST']
+password = os.environ['DATABASE_PASSWORD']
+database_type = os.environ['DATABASE_TYPE']
+
+SQLALCHEMY_DATABASE_URL = f"{database_type}://{username}:{password}@{host}/{database}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
