baf9d7a8dd371276262c2b6907ea07f9da97790a
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-13 02:48:46 +0200

    Added username , first_name and last_name to user Base model

--- backend/mms_app_backend/mms_app_backend/api/authentication/schemas.py
+++ backend/mms_app_backend/mms_app_backend/api/authentication/schemas.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel,EmailStr
 
 class UserBase(BaseModel):
     email: EmailStr
+    username: str
+    first_name: str
+    last_name: str
 
 
 class UserCreate(UserBase):
