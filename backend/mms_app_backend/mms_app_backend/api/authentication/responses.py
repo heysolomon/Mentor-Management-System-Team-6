4b9be37824cfef7519d91a49310fb1673483eaff
Albert Byrone <albertbyrone1677@gmail.com>
2023-04-13 17:13:24 +0300

    create the login user response class

--- backend/mms_app_backend/mms_app_backend/api/authentication/responses.py
+++ backend/mms_app_backend/mms_app_backend/api/authentication/responses.py
@@ -6,7 +6,11 @@ from ..utils import ResponseModel
 
 class UserData(TypedDict):
     user: User
+    access_token: str
 
 
 class CreateUserResponse(ResponseModel):
+    data: UserData | EmptyDict = {}
+    
+class LoginUserResponse(ResponseModel):
     data: UserData | EmptyDict = {}
