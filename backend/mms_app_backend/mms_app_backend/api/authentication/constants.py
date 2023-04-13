5d0b1755f15b5d1451cbb765a56919298cd6f258
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-13 03:33:08 +0200

    completed basic user sign up crud.

--- backend/mms_app_backend/mms_app_backend/api/authentication/constants.py
+++ backend/mms_app_backend/mms_app_backend/api/authentication/constants.py
@@ -3,4 +3,4 @@ from passlib.context import CryptContext
 PASSWORD_CONTEXT = CryptContext(schemes=['bcrypt'])
 USED_EMAIL_MESSAGE = "An account with that email already exists"
 ACCOUNT_CREATED_MESSAGE = "User Account has been created successfully "
-USER_USERNAME_MESSAGE = "An account with that username already exists"
+USED_USERNAME_MESSAGE = "An account with that username already exists"
