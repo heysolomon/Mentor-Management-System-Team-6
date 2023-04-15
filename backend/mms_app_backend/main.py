9b3d11370a7218c14c3e628403bd9649254e766a
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-15 05:28:46 +0200

    added conditional imports to main

--- backend/mms_app_backend/main.py
+++ backend/mms_app_backend/main.py
@@ -2,14 +2,18 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from mms_app_backend.configs.database_config import Base, engine
+
 
 # Conditional import due to pytest which imports tests as external packages.
 if __name__ == "__main__":
     from mms_app_backend.api.authentication.route import router as auth_router
+    from mms_app_backend.api.account_management.models import Profile
+    from mms_app_backend.configs.database_config import Base, engine
 else:
     from .mms_app_backend.api.authentication.route import router as auth_router
-from  mms_app_backend.api.account_management.models import Profile
+    from .mms_app_backend.api.account_management.models import Profile
+    from .mms_app_backend.configs.database_config import Base, engine
+
 
 Base.metadata.create_all(bind=engine)
 
