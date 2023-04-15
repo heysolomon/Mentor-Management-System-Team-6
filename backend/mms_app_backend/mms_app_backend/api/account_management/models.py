32767a686fef63dc19a71089ba5addec4e533c3d
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-15 03:44:14 +0200

    configured mentor manager and location to return only one value.

--- backend/mms_app_backend/mms_app_backend/api/account_management/models.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/models.py
@@ -11,10 +11,9 @@ class Profile(AbstractBaseModel):
     about = Column(Text)
     website = Column(URLType)
     social_links = relationship("SocialLink", back_populates='profile')
-    locations = relationship("Location", back_populates='profiles')
-    programs = relationship("Program", back_populates='profiles')
-    mentors = relationship("Mentor", back_populates='profiles')
-    mentor_managers = relationship("MentorManager", back_populates='profiles')
+    location = relationship("Location", back_populates='profile',uselist=False)
+    mentor = relationship("Mentor", back_populates='profile',uselist=False)
+    mentor_manager = relationship("MentorManager", back_populates='profile',uselist=False)
     user = relationship("User", back_populates='profile')
     user_id = Column(Integer, ForeignKey("users.id"))
 
@@ -29,33 +28,31 @@ class SocialLink(AbstractBaseModel):
 
 class Location(AbstractBaseModel):
     __tablename__ = 'locations'
-    id = Column(Integer, primary_key=True)
     city = Column(String)
     state = Column(String)
     country = Column(String)
     profile_id = Column(Integer, ForeignKey('profiles.id'))
-    profile = relationship("Profile", back_populates='locations')
+    profile = relationship("Profile", back_populates='location')
 
 
 class Program(AbstractBaseModel):
     __tablename__ = 'programs'
-    id = Column(Integer, primary_key=True)
     name = Column(String)
     avatar = Column(URLType)
     description = Column(Text)
-    mentor_manager_id = Column(Integer, ForeignKey('profiles.id'))
-    mentor_manager = relationship("MentorManager", back_populates='program')
-    mentee_id = Column(Integer, ForeignKey('profiles.id'))
-    mentee = relationship("Profile", back_populates='programs')
     mentors = relationship("Mentor", back_populates='program')
     criteria = relationship("Criterion", back_populates='program')
 
 
 class Mentor(AbstractBaseModel):
     __tablename__ = 'mentors'
-    id = Column(Integer, primary_key=True)
     about = Column(Text)
     profile_id = Column(Integer, ForeignKey('profiles.id'))
     profile = relationship("Profile", back_populates='mentors')
+    program = relationship("Program", back_populates='mentors')
+    program_id = Column(Integer, ForeignKey('programs.id'))
 
-
+class MentorManager(AbstractBaseModel):
+    __tablename__ ='mentor_managers'
+    profile_id = Column(Integer, ForeignKey('profiles.id'))
+    profile = relationship("Profile", back_populates='mentor_manager')
