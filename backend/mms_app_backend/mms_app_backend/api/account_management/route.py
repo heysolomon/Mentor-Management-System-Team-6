6b348ff861b2bd42aaf2f1efb8e59159fd7aab59
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-16 06:11:08 +0200

    completed access token validation.

--- backend/mms_app_backend/mms_app_backend/api/account_management/route.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/route.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, status, Depends
-
+from fastapi import APIRouter, status, Depends,Response
 from .crud import create_profile
 from .schemas import CreateProfile
 from ..authentication.helpers import verify_access_token
 from ..utils import get_token, get_db
 from .responses import CreateProfileResponse
+from ..authentication.constants import INVALID_ACCESS_TOKEN_MESSAGE
 router = APIRouter()
 
 get = router.get
@@ -12,7 +12,7 @@ post = router.post
 
 
 @post("/create-profile", status_code=status.HTTP_201_CREATED, response_model=CreateProfileResponse)
-async def create_profile(profile: CreateProfile, jwt_token=Depends(get_token), db=Depends(get_db)):
+async def create_profile(profile: CreateProfile,response:Response, jwt_token=Depends(get_token), db=Depends(get_db)):
     """
     Create a new profile for a user after they have successfully signed up to the system.
     """
@@ -20,7 +20,9 @@ async def create_profile(profile: CreateProfile, jwt_token=Depends(get_token), d
     user = verify_access_token(db, jwt_token)
     if not user:
         profile_response.success = False
-
-    created_profile = create_profile(profile)
+        profile_response.message = INVALID_ACCESS_TOKEN_MESSAGE
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return profile_response
+    created_profile = create_profile(profile,user)
 
     return profile
