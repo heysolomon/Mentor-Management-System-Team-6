a8792dbca21acf13639b1293862e82d44c0ae237
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-16 06:05:43 +0200

    Removed unused imports . completed profile data response. Added invalid access token message.

--- backend/mms_app_backend/mms_app_backend/api/account_management/route.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/route.py
@@ -4,18 +4,23 @@ from .crud import create_profile
 from .schemas import CreateProfile
 from ..authentication.helpers import verify_access_token
 from ..utils import get_token, get_db
-
+from .responses import CreateProfileResponse
 router = APIRouter()
 
 get = router.get
 post = router.post
 
 
-@post("/create-profile", status_code=status.HTTP_201_CREATED, response_model=CreateProfile)
+@post("/create-profile", status_code=status.HTTP_201_CREATED, response_model=CreateProfileResponse)
 async def create_profile(profile: CreateProfile, jwt_token=Depends(get_token), db=Depends(get_db)):
+    """
+    Create a new profile for a user after they have successfully signed up to the system.
+    """
+    profile_response = CreateProfileResponse()
     user = verify_access_token(db, jwt_token)
     if not user:
-        pass
+        profile_response.success = False
+
     created_profile = create_profile(profile)
 
     return profile
