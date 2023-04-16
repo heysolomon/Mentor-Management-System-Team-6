73536a5d88781ecaa3bb5acc570c0f1b2aa6150c
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-16 05:26:51 +0200

    added get token util.

--- backend/mms_app_backend/mms_app_backend/api/account_management/route.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/route.py
@@ -1,15 +1,15 @@
-from fastapi import APIRouter, status
-
+from fastapi import APIRouter, status,Depends
+from fastapi.security import OAuth2PasswordBearer
 from .crud import create_profile
 from .schemas import CreateProfile
 
 router = APIRouter()
-
+get_token = OAuth2PasswordBearer
 get = router.get
 post = router.post
 
 
 @post("/create-profile", status_code=status.HTTP_201_CREATED, response_model=CreateProfile)
-async def create_profile(profile: CreateProfile):
+async def create_profile(profile: CreateProfile,jwt_token=Depends(get_token)):
     created_profile = create_profile(profile)
     return profile
--- backend/mms_app_backend/mms_app_backend/api/utils.py
+++ backend/mms_app_backend/mms_app_backend/api/utils.py
@@ -1,7 +1,7 @@
 from typing import TypedDict
 
 from pydantic import BaseModel
-
+from fastapi.security import OAuth2PasswordBearer
 from ..configs.database_config import SessionLocal
 
 
@@ -12,6 +12,8 @@ def get_db():
     finally:
         db.close()
 
+def get_token():
+    return OAuth2PasswordBearer(tokenUrl='Token')
 
 class ResponseModel(BaseModel):
     success: bool = False
