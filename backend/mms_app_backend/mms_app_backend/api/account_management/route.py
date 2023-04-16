b97151e926345a6e974f7edc3a23df7885505577
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-16 05:46:05 +0200

    completed authentication using jwt.

--- backend/mms_app_backend/mms_app_backend/api/account_management/route.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/route.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, status, Depends
 
 from .crud import create_profile
 from .schemas import CreateProfile
-from ..utils import get_token
+from ..authentication.helpers import verify_access_token
+from ..utils import get_token, get_db
 
 router = APIRouter()
 
@@ -11,7 +12,10 @@ post = router.post
 
 
 @post("/create-profile", status_code=status.HTTP_201_CREATED, response_model=CreateProfile)
-async def create_profile(profile: CreateProfile, jwt_token=Depends(get_token)):
+async def create_profile(profile: CreateProfile, jwt_token=Depends(get_token), db=Depends(get_db)):
+    user = verify_access_token(db, jwt_token)
+    if not user:
+        pass
     created_profile = create_profile(profile)
 
     return profile
