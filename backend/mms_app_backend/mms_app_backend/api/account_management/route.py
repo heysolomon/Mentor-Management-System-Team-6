7203270620f228707f35e672d51d9808cda33f7d
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-16 05:29:51 +0200

    Updated imports in account management route.py

--- backend/mms_app_backend/mms_app_backend/api/account_management/route.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/route.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, status,Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, status, Depends
+
 from .crud import create_profile
 from .schemas import CreateProfile
+from ..utils import get_token
 
 router = APIRouter()
-get_token = OAuth2PasswordBearer
+
 get = router.get
 post = router.post
 
 
 @post("/create-profile", status_code=status.HTTP_201_CREATED, response_model=CreateProfile)
-async def create_profile(profile: CreateProfile,jwt_token=Depends(get_token)):
+async def create_profile(profile: CreateProfile, jwt_token=Depends(get_token)):
     created_profile = create_profile(profile)
     return profile
