f8805194f0e1d844272305ef4a4dcb48cec42cfc
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-16 06:12:09 +0200

    completed crud operation.

--- backend/mms_app_backend/mms_app_backend/api/account_management/crud.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/crud.py
@@ -1,7 +1,8 @@
 from .models import Profile
 
 
-def create_profile(db, profile, user_id):
+def create_profile(db, profile, user):
+    user_id = user.id
     profile = Profile(**profile, user_id=user_id)
     db.session.add(profile)
     db.session.commit()
