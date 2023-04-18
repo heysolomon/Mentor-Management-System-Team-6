b3549407c214e0a37db398310b5cef9455c6d3db
Vic.Orlands <chimezieinnocent39@gmail.com>
2023-04-18 15:18:48 +0100

    update: Added Archive page

--- frontend/mms-Admin/src/pages/Dashboard/Settings/Privacy.jsx
+++ frontend/mms-Admin/src/pages/Dashboard/Settings/Privacy.jsx
@@ -5,29 +5,29 @@ export default function Privacy() {
   return (
     <div className="faqs border-[1px] rounded-[5px] border-black9 mx-10 p-5">
       <div className="flex items-center justify-between w-[40%] mb-5">
-        <h3 className="text-black2 font-2xl font-semibold">
+        <h3 className="text-black2 font-semibold">
           Show contact info
         </h3>
         <Toggle enabled={false} />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
-        <h3 className="text-black2 font-2xl font-semibold">Show GitHub</h3>
+        <h3 className="text-black2 font-semibold">Show GitHub</h3>
         <Toggle enabled={true} />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
-        <h3 className="text-black2 font-2xl font-semibold">Show Instagram</h3>
+        <h3 className="text-black2 font-semibold">Show Instagram</h3>
         <Toggle enabled={false} />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
-        <h3 className="text-black2 font-2xl font-semibold">Show Linkedin</h3>
+        <h3 className="text-black2 font-semibold">Show Linkedin</h3>
         <Toggle enabled={true} />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
-        <h3 className="text-black2 font-2xl font-semibold">Show Twitter</h3>
+        <h3 className="text-black2 font-semibold">Show Twitter</h3>
         <Toggle enabled={false} />
       </div>
     </div>
