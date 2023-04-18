ab02456a6b6826af19b0d62dde794f38e1a8321f
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-18 04:07:25 +0200

    Fixed codacity style complaints .

--- backend/mms_app_backend/mms_app_backend/api/account_management/responses.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/responses.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from fastapi_camelcase import CamelModel
 
 from .schemas import ViewProfile
 from ..utils import ResponseModel
-from fastapi_camelcase import CamelModel
+
 
 class ProfileData(CamelModel):
-    profile : ViewProfile|None
+    profile: ViewProfile | None
 
 
 class CreateProfileResponse(ResponseModel):
-    data : ProfileData = ProfileData()
+    data: ProfileData = ProfileData()
