df963f9bee32b17f82a87d6358681e39991ed99b
Vic.Orlands <chimezieinnocent39@gmail.com>
2023-04-18 16:02:06 +0100

    update: Added icons

--- frontend/mms-Admin/src/pages/Dashboard/Settings/Privacy.jsx
+++ frontend/mms-Admin/src/pages/Dashboard/Settings/Privacy.jsx
@@ -5,15 +5,13 @@ export default function Privacy() {
   return (
     <div className="faqs border-[1px] rounded-[5px] border-black9 mx-10 p-5">
       <div className="flex items-center justify-between w-[40%] mb-5">
-        <h3 className="text-black2 font-semibold">
-          Show contact info
-        </h3>
+        <h3 className="text-black2 font-semibold">Show contact info</h3>
         <Toggle enabled={false} />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
         <h3 className="text-black2 font-semibold">Show GitHub</h3>
-        <Toggle enabled={true} />
+        <Toggle enabled />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
@@ -23,7 +21,7 @@ export default function Privacy() {
 
       <div className="flex items-center justify-between w-[40%] mb-5">
         <h3 className="text-black2 font-semibold">Show Linkedin</h3>
-        <Toggle enabled={true} />
+        <Toggle enabled />
       </div>
 
       <div className="flex items-center justify-between w-[40%] mb-5">
