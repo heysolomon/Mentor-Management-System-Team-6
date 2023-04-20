3be098a3200e1da29279fbec9339865d1bc92376
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-20 05:29:38 +0200

    Configured alembic initial revision.

--- backend/mms_app_backend/mms_app_backend/configs/alembic_config/env.py
+++ backend/mms_app_backend/mms_app_backend/configs/alembic_config/env.py
@@ -1,12 +1,12 @@
+import sys,os
 from logging.config import fileConfig
-from alembic.config import  Config
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from ...database_config import db_url
 from alembic import context
-alembic_config = Config('alembic.ini')
-
-alembic_config.set_main_option('sqlalchemy.url', db_url)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+from database_config import db_url,Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,12 +16,12 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
+config.set_main_option("sqlalchemy.url", db_url)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
