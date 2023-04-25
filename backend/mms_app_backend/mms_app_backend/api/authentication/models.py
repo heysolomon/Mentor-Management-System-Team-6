f3c3404599f1ad3add1d8c1380b06ec653028641
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-25 18:54:23 +0200

    Created the ResetToken model

--- backend/mms_app_backend/mms_app_backend/api/authentication/models.py
+++ backend/mms_app_backend/mms_app_backend/api/authentication/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, String
+from sqlalchemy import Boolean, Column, String,ForeignKey,Integer
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType
 
@@ -14,3 +14,9 @@ class User(AbstractBaseModel):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     profile = relationship("Profile", back_populates='user')
+    password_reset_token = relationship("PasswordResetToken", back_populates="user")
+## Token sent to user email when resetting password
+class PasswordResetToken(AbstractBaseModel):
+    __tablename__ = "password_reset_tokens"
+    user_id = Column(Integer,ForeignKey("users.id"))
+    user = relationship("User", back_populates='password_reset_token')
