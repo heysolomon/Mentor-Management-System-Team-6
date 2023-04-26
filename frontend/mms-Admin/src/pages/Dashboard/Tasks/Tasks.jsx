d9a30250773408dcec33fd1c85f3913fe691d957
dinatahoun92 <dinatahoun992@gmail.com>
2023-04-26 02:13:45 +0200

    task panel

--- frontend/mms-Admin/src/pages/Dashboard/Tasks/Tasks.jsx
+++ frontend/mms-Admin/src/pages/Dashboard/Tasks/Tasks.jsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import './Tasks.css';
 import { RiSearchLine } from 'react-icons/ri';
 import { BsFilter } from 'react-icons/bs';
+import { BiArrowBack } from 'react-icons/bi';
 import { GoCalendar } from 'react-icons/go';
 import taskImg from './task.png';
+import taskIcon1 from './taskIcon1.png';
+import taskIcon2 from './taskIcon2.png';
+import taskIcon3 from './taskIcon3.png';
 import styles from '../styles/dashboard.module.css';
 
 function Tasks() {
@@ -12,30 +16,34 @@ function Tasks() {
 
   const search = () => {
     setChecked(true);
-    const timer = setTimeout(() => {
-      setChecked(false);
-    }, 3000);
-    return () => clearTimeout(timer);
   };
 
   return (
     <div className="flex flex-row ">
-      <div className={`basis-2/5 flex m-5 flex-col overflow-y-auto pb-5 h-screen ${styles.scroll}`}>
+      <div className={`basis-1/3 flex m-5 flex-col overflow-y-auto pb-5 h-screen ${styles.scroll}`}>
         <div className="tasksHeader flex flex-row">
-          <h1 className="font-[600] tasksH grow">Tasks</h1>
           {checked ? (
-            <input
-              type="text"
-              className="focus:outline-none bg-transparent pl-4 w-full"
-              placeholder="Search tasks"
-              onKeyDown={search}
-            />
-          ) : <RiSearchLine className="text-teal-700 text-xl mx-2 cursor-pointer" onClick={search} />}
+            <div className="flex flex-row-reverse">
+              <input
+                type="text"
+                className="focus:outline-none bg-transparent pl-4 w-full"
+                placeholder="Search tasks"
+                onKeyDown={search}
+              />
+              <BiArrowBack className="text-teal-700 text-2xl mx-2 cursor-pointer" onClick={() => setChecked(false)} />
+            </div>
+          ) : (
 
-          <BsFilter className={`text-teal-700 text-2xl mx-2 cursor-pointer ${sort ? 'rotate-180' : ''}`} onClick={() => setSort(!sort)} />
+            <>
+              <h1 className="font-[600] tasksH grow">Tasks</h1>
+              <RiSearchLine className="text-teal-700 text-xl mx-2 cursor-pointer" onClick={search} />
+              <BsFilter className={`text-teal-700 text-2xl mx-2 cursor-pointer ${sort ? 'rotate-180' : ''}`} onClick={() => setSort(!sort)} />
+            </>
+          )}
+
         </div>
         {/* start tasks */}
-        <div className="taskContainer m-5">
+        <div className="taskContainer me-5">
 
           {
           Array.from(Array(10)).map((i) => (
@@ -45,10 +53,9 @@ function Tasks() {
                 <h3 className="font-semibold">Room Library article write...</h3>
                 <div className="taskdate flex">
 
-                  <GoCalendar className="text-teal-700 text-2xl me-3" />
-                  <p className="text-xs text-gray-600 font-light align-middle pt-1">3 days from now</p>
+                  <GoCalendar className="text-teal-700 text-l me-3" />
+                  <p className="text-xs text-gray-600 font-light align-middle  align-middle">3 days from now</p>
                 </div>
-
               </div>
             </div>
 
@@ -57,7 +64,45 @@ function Tasks() {
         </div>
         {/* end tasks */}
       </div>
-      <div className="basis-3/5">03</div>
+      <div className="basis-2/3">
+        <div className="flex flex-row-reverse">
+          <button
+            type="submit"
+            className="bg-pri3 py-2.5 px-10 rounded-md text-white font-semibold"
+          >
+            Create New Task
+          </button>
+        </div>
+        <div className="task flex  flex-col m-3  rounded-md  border-2 border-grey-400 w-full">
+
+          <div className="flex flex-row p-4">
+            <img src={taskImg} alt="icon" className="object-contain" />
+            <div className="rightTask ms-8">
+              <h3 className="font-semibold ">Room library article written in java</h3>
+              <div className="taskdate flex items-center">
+
+                <GoCalendar className="text-teal-700 text-l me-3" />
+                <p className="text-xs text-gray-400 font-light align-middle align-middle">3 days from now</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-pri11 rounded-b-lg p-4">
+            <p className="text-gray-500 pt-2">
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.
+              Pellentesque sit amet sapien fringilla,
+              mattis ligula consectetur, ultrices mauris.
+              Maecenas vitae mattis tellus. Nullam quis imperdiet augue.
+              Vestibulum auctor ornare leo, non suscipit magna interdum eu.
+              Curabitur pellentesque nibh nibh,
+              at maximus ante fermentum sit amet. Pellentesque
+            </p>
+
+            <div className="flex" />
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
