8508d593ce9eabce40a2d0399a5039a40f23920d
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-04-27 15:54:43 +0200

    Minor formatting in Task Responses.

--- backend/mms_app_backend/mms_app_backend/api/tasks/responses.py
+++ backend/mms_app_backend/mms_app_backend/api/tasks/responses.py
@@ -1,10 +1,12 @@
+from fastapi_camelcase import CamelModel
+
 from .schemas import GetTask
 from ..utils import ResponseModel
-from fastapi_camelcase import CamelModel
+
+
 class CreateTaskData(CamelModel):
-    task : GetTask | None
+    task: GetTask | None
+
 
 class CreateTaskResponse(ResponseModel):
     data: CreateTaskData = CreateTaskData()
-
-
