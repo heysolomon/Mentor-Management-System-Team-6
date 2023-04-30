4ccecb11d324af8d127d394b1c7c03ede4d0ec17
Albert Byrone <albertbyrone1677@gmail.com>
2023-04-30 14:07:49 +0300

    create a relationship between the task table and the user table

--- backend/mms_app_backend/mms_app_backend/api/tasks/models.py
+++ backend/mms_app_backend/mms_app_backend/api/tasks/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Column, Text, Boolean
+from sqlalchemy import String, Column, Text, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 from ..models import AbstractBaseModel
@@ -12,3 +12,5 @@ class Task(AbstractBaseModel):
     mentors = relationship("Mentor", back_populates='task')
     completed = Column(Boolean, default=False)
     open = Column(Boolean, default=True)
+    assigned_to_id = Column(Integer, ForeignKey('users.id')) #Add a foreign key that will refer to the ID column in the User table
+    assigned_to = relationship('User',back_populates='tasks') # Create a bidirectional relationship between the two tables
