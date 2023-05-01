7028c09bef2a2d9ed8cc44ce832227675fc48dd2
Albert Byrone <albertbyrone1677@gmail.com>
2023-05-01 21:16:08 +0300

    remove unused import

--- backend/mms_app_backend/mms_app_backend/api/tasks/models.py
+++ backend/mms_app_backend/mms_app_backend/api/tasks/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Column, Text, Boolean, ForeignKey
+from sqlalchemy import String, Column, Text, Boolean
 from sqlalchemy.orm import relationship
 
 from ..models import AbstractBaseModel
