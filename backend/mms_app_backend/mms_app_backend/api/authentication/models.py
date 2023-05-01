d27cfb0ab7b73e1a2a60ded41975f6b0c661a066
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-05-01 16:33:05 +0200

    Created User conversation models and assosciations.

--- backend/mms_app_backend/mms_app_backend/api/authentication/models.py
+++ backend/mms_app_backend/mms_app_backend/api/authentication/models.py
@@ -15,6 +15,8 @@ class User(AbstractBaseModel):
     is_active = Column(Boolean, default=True)
     profile = relationship("Profile", back_populates='user')
     password_reset_token = relationship("PasswordResetToken", back_populates="user")
+    conversations = relationship("Conversation", back_populates='participants', secondary="participant_conversation_association")
+    created_conversations = relationship("Conversation", back_populates='creator')
 
 
 # Token sent to user email when resetting password
