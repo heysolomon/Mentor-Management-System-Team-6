3284486c147d196477cee1475b11e76210260b41
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-05-02 00:27:26 +0200

    Completed conversation responses.py

---
+++ backend/mms_app_backend/mms_app_backend/api/messages/responses.py
@@ -0,0 +1,12 @@
+from fastapi_camelcase import CamelModel
+
+from .schemas import ViewConversation
+from ..utils import ResponseModel
+
+
+class ConversationData(CamelModel):
+    conversation = ViewConversation | None
+
+
+class ConversationResponse(ResponseModel):
+    data: ConversationData = ConversationData()
