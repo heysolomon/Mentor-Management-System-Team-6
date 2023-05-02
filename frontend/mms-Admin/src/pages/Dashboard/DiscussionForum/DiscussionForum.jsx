bd16fd767d098326ede41957fc3e8095fc1cf722
Solomon Akuson <akusonsolomon15@gmail.com>
2023-05-03 00:28:00 +0100

    fixed discussion forum header

--- frontend/mms-Admin/src/pages/Dashboard/DiscussionForum/DiscussionForum.jsx
+++ frontend/mms-Admin/src/pages/Dashboard/DiscussionForum/DiscussionForum.jsx
@@ -5,14 +5,16 @@ import DiscussionPost from '../../../components/Dashboard/DiscussionForum/Discus
 function DiscussionForum() {
   return (
     <div className="pb-2">
-      <h2 className="text-[24px] font-[600] text-black1 mb-[20px]">
-        Discussion Forum
-      </h2>
-      <div className="h-[48px] rounded-[5px] border-[1px] border-black9 flex justify-between items-center px-5 mb-[33px]">
-        <p className="text-black5 font-[400] text-[20px]">Add new topic</p>
-        <button type="button">
-          <CircleAddIcon color="#058B94" />
-        </button>
+      <div className="sticky w-full bg-white top-[80px] mb-[33px]">
+        <h2 className="text-[24px] font-[600] text-black1 mb-[20px]">
+          Discussion Forum
+        </h2>
+        <div className="h-[48px] rounded-[5px] border-[1px] border-black9 flex justify-between items-center px-5">
+          <p className="text-black5 font-[400] text-[20px]">Add new topic</p>
+          <button type="button">
+            <CircleAddIcon color="#058B94" />
+          </button>
+        </div>
       </div>
 
       <div className="">
