3fa4ec8710c6aca33c034b751fcb35c9994ec363
Solomon Akuson <akusonsolomon15@gmail.com>
2023-05-02 20:33:41 +0100

    started backend integration

--- frontend/mms-Admin/src/pages/Dashboard/Settings/SettingChangePassword.jsx
+++ frontend/mms-Admin/src/pages/Dashboard/Settings/SettingChangePassword.jsx
@@ -1,42 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { SpinnerCircular } from 'spinners-react';
 import * as Yup from 'yup';
 import FormikForm from '../../../components/FormikForm/FormikForm';
 import InputField from '../../../components/InputField';
 import Button from '../../../components/utilities/Buttons/Button';
+import { changePasswordFailure,
+  changePasswordStart,
+  changePasswordSuccess } from '../../../redux/features/userSlice';
+import { api } from '../../../services/api';
 
 function SettingChangePassword() {
   const initialValues = {
     oldPassword: '',
     newPassword: '',
-    confirmPassword: '',
   };
 
+  // the validation schema for the forgot password
   const validate = Yup.object({
     oldPassword: Yup.string()
       .min(8, 'Password must be atleast 8 characters long')
-      .required('Current Password is required')
-      .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*)[A-Za-z\d]{8,}$/,
-        `Must Contain 8 Characters, One Uppercase, One Lowercase,
-        One Number and one special case Character`,
-      ),
+      .required('Current Password is required'),
     newPassword: Yup.string()
       .min(8, 'Password must be atleast 8 characters long')
       .required('New Password is required')
       .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*)[A-Za-z\d]{8,}$/,
-        `Must Contain 8 Characters, One Uppercase, One Lowercase,
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+        `Must Contain One Uppercase, One Lowercase,
         One Number and one special case Character`,
       ),
     confirmPassword: Yup.string()
       .oneOf([Yup.ref('newPassword'), null], 'New Password must match')
       .required('Confirm Password is required'),
   });
+
+  const [message, setMessage] = useState('');
+  const dispatch = useDispatch();
+
+  const { userInfo, changePassword, changePasswordError } = useSelector(
+    (state) => state.user,
+  );
+
+  // logged in user's id
+  const userId = userInfo.data.user.id;
+  const changeUserPassword = async (values) => {
+    dispatch(changePasswordStart());
+    try {
+      const changeUserPasswordRequest = await api.patch(`/${userId}/password`, {
+        ...values,
+      });
+      console.log(changeUserPasswordRequest);
+      dispatch(changePasswordSuccess(changeUserPasswordRequest));
+      // setMessage(user.data.message);
+    } catch (err) {
+      if (err) {
+        dispatch(changePasswordFailure());
+        console.log(err);
+        setMessage(err.response.data.detail);
+      }
+    }
+  };
+
+  const submit = async (values) => {
+    changeUserPassword(values);
+  };
   return (
     <div className="border-[1px] rounded-[5px] border-black9 mx-10 p-5">
       <FormikForm
         initialValues={initialValues}
         validationSchema={validate}
+        submit={submit}
       >
         {/* old password */}
         <div className="flex items-center">
@@ -83,8 +117,27 @@ function SettingChangePassword() {
           />
         </div>
 
+        <p
+          className={`font-[400] text-black5 font-mukta text-[16px] mt-[20px] ${
+            changePasswordError ? 'text-red-500' : 'text-pri2'
+          }`}
+        >
+          {message}
+        </p>
+
         <div className="flex justify-end mt-[30px]">
-          <Button width="w-full md:w-[35%]">Save new password</Button>
+          <Button width="w-full md:w-[35%]">
+            {changePassword ? (
+              <SpinnerCircular
+                color="#F7FEFF"
+                className="mr-2"
+                thickness={250}
+                size={20}
+              />
+            ) : (
+              ' Save new password'
+            )}
+          </Button>
         </div>
         <div className="flex justify-center mt-[30px]">
           <p className="font-[600] text-[18px] text-[#023C40] underline">
