d386aa405675f71ed5aa64cd24edb26bfb0dc9c4
Solomon Akuson <akusonsolomon15@gmail.com>
2023-05-03 00:07:50 +0100

    added the comments section

--- frontend/mms-Admin/src/components/Dashboard/DiscussionForum/DiscussionCommentSection.jsx
+++ frontend/mms-Admin/src/components/Dashboard/DiscussionForum/DiscussionCommentSection.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 import { AttachmentIcon, EmojiIcon } from '../../../assets/images';
+import DiscussionComment from './DiscussionComment';
 
 function DiscussionCommentSection() {
+  const comments = [
+    {
+      id: 1,
+      name: 'Ibrahim Kekule',
+      comment:
+        'Found this so insightful. please how can i register to be a part of the program?',
+      time: 'Just now',
+    },
+    {
+      id: 2,
+      name: 'Sarah Tasha',
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enilf.',
+      time: '12 mins ago',
+    },
+    {
+      id: 3,
+      name: 'Ibrahim Kekule',
+      comment:
+        'Found this so insightful. please how can i register to be a part of the program?',
+      time: '12 mins ago',
+    },
+    {
+      id: 4,
+      name: 'Sarah Tasha',
+      comment:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enilf.',
+      time: '25mins ago',
+    },
+  ];
   return (
     <div className="">
       <div className="mt-[41px] w-full bg-pri11 h-[120px] border-pri10 border-[1px] rounded-[5px] px-[20px] py-[16px] flex flex-col justify-between">
@@ -30,7 +61,16 @@ function DiscussionCommentSection() {
       </div>
 
       {/* comments */}
-      <div className="border-black9 rounded-[10px] h-full p-[20px]">sjsjj</div>
+      <div className="border-[1px] border-black9 rounded-[10px] h-full px-[20px] pb-[20px] min-h-[40px] w-full mt-[23px]">
+        {comments.map((comment) => (
+          <DiscussionComment
+            key={comment.id}
+            name={comment.name}
+            body={comment.comment}
+            time={comment.time}
+          />
+        ))}
+      </div>
     </div>
   );
 }
