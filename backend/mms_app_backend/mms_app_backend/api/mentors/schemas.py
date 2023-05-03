88b98d99929ec8a003397836280a835f131dc214
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-05-03 17:32:18 +0200

    Removed schemas comments.

--- backend/mms_app_backend/mms_app_backend/api/mentors/schemas.py
+++ backend/mms_app_backend/mms_app_backend/api/mentors/schemas.py
@@ -1,13 +1,7 @@
 from fastapi_camelcase import CamelModel
 
 
-# about = Column(Text)
-# profile_id = Column(Integer, ForeignKey('profiles.id'))
-# profile = relationship("Profile", back_populates='mentor')
-# programs = relationship("Program", back_populates='mentors', secondary="program_mentor_association")
-# roles = relationship("Role", back_populates='mentor')
-# task = relationship("Task", back_populates='mentors')
-# task_id = Column(Integer, ForeignKey('tasks.id'))
+
 
 
 class BaseMentors(CamelModel):
