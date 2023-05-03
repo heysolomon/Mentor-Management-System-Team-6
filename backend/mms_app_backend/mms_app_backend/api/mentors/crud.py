5424df19b44ef232736a2132d6050f07e3dd9a61
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-05-03 16:07:52 +0200

    Completed get mentors crud.

---
+++ backend/mms_app_backend/mms_app_backend/api/mentors/crud.py
@@ -0,0 +1,17 @@
+from ..account_management.models import Mentor
+
+
+def get_mentors_crud(db):
+    mentors = db.query(Mentor).all()
+    processed_mentors = []
+    if mentors:
+        for mentor in mentors:
+            if mentor.programs:
+                programs = [program.id for program in mentor.programs]
+                mentor.programs = programs
+            if mentor.roles:
+                roles = [role.id for role in mentor.roles]
+                mentor.roles = roles
+        processed_mentors.append(mentor)
+
+    return processed_mentors
