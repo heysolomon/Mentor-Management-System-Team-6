23910fc5dbe1c320dcca1e5b861241986241a0d9
Munalula-Sikazwe <munalualsikazwe67@gmail.com>
2023-05-09 00:49:09 +0200

    Completed About model first pass.

--- backend/mms_app_backend/mms_app_backend/api/account_management/models.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy_utils import URLType
 
 from ..models import AbstractBaseModel
+from ...configs.database_config import Base
 
 
 class Profile(AbstractBaseModel):
@@ -50,6 +51,7 @@ class Program(AbstractBaseModel):
     mentor_manager = relationship("MentorManager", back_populates='programs')
     mentor_manager_id = Column(Integer, ForeignKey('mentor_managers.id'))
     criteria = relationship("Criterion", back_populates='program')
+    previous_abouts = relationship("About", back_populates='previous_programs', secondary="about_programs_associations")
 
 
 class Mentor(AbstractBaseModel):
@@ -89,10 +91,19 @@ class Role(AbstractBaseModel):
     __tablename__ = 'roles'
     name = Column(String)
     description = Column(Text)
-    mentor = relationship("Mentor", back_populates='roles')
-    mentor_id = Column(Integer, ForeignKey('mentors.id'))
-    mentor_manager = relationship("MentorManager", back_populates='roles')
-    mentor_manager_id = Column(Integer, ForeignKey('mentor_managers.id'))
+    abouts = relationship("About", back_populates='roles', secondary="about_role_associations")
+
+
+class AboutRoleAssociation(Base):
+    __tablename__ = 'about_role_associations'
+    about_id = Column(Integer, ForeignKey('abouts.id'))
+    role = Column(Integer, ForeignKey('role.id'))
+
+
+class AboutProgramsAssociation(Base):
+    __tablename__ = 'about_programs_associations'
+    program_id = Column(Integer, ForeignKey('programs.id'))
+    about_id = Column(Integer, ForeignKey('abouts.id'))
 
 
 class About(AbstractBaseModel):
@@ -101,9 +112,17 @@ class About(AbstractBaseModel):
     was_mentor = Column(Boolean, default=False)
     availability = Column(Boolean, default=True)
     experience_years = Column(Integer)
+    previous_programs = relationship("Program", back_populates='previous_abouts',
+                                     secondary='about_programs_associations')
+    program = relationship("Program", back_populates='mentor_about')
+    program_id = Column(Integer, ForeignKey("programs.id"))
+    documents = relationship("Document", back_populates='about')
+    roles = relationship("Role", back_populates='abouts', secondary='about_role_associations')
 
 
 class Document(AbstractBaseModel):
     __tablename__ = 'documents'
     name = Column(String(256))
     url = Column(URLType)
+    about = relationship("About", back_populates='documents')
+    about_id = Column(Integer, ForeignKey('abouts.id'))
