ce262e673cc30218111c8a0f6168867f23f3a643
Albert Byrone <albertbyrone1677@gmail.com>
2023-05-11 12:49:19 +0300

    remove unused imports

--- backend/mms_app_backend/mms_app_backend/api/account_management/models.py
+++ backend/mms_app_backend/mms_app_backend/api/account_management/models.py
@@ -1,5 +1,4 @@
-import datetime
-from sqlalchemy import Column, Integer, Text, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, Text, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils import URLType
 
