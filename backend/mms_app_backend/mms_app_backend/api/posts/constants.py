a2e96ed4966fb3054f3eb2768182efef84e43f86
Albert Byrone <albertbyrone1677@gmail.com>
2023-05-11 09:11:52 +0300

    add messages to be displayed

---
+++ backend/mms_app_backend/mms_app_backend/api/posts/constants.py
@@ -0,0 +1,6 @@
+CREATED_POST_SUCCESSFUL_MESSAGE = "The post was created successfully"
+GET_POST_SUCCESSFUL_MESSAGE = "The post were successfully retrieved"
+POST_NOT_FOUND_MESSAGE = "The post was not found"
+UPDATE_POST_SUCCESSFUL_MESSAGE = "The post was successfully updated"
+POSTS_NOT_FOUND_MESSAGE = "A task with the provided id does not exist"
+POST_DELETED_SUCCESSFUL_MESSAGE = "The task was successfully deleted"
